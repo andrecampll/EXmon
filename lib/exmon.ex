916c3045f691bb3a6c80ace9489b7ac5841d7ec6
André <andrevictor50@gmail.com>
2020-12-03 10:33:15 -0300

    adding game agent

--- lib/exmon.ex
+++ lib/exmon.ex
@@ -1,7 +1,15 @@
 defmodule Exmon do
-  alias Exmon.Player
+  alias Exmon.{Game,Player}
+
+  @computer_name "Robot"
 
   def create_player(move_rnd, move_avg, move_heal, name ) do
     Player.build(move_rnd, move_avg, move_heal, name)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :real)
+    |> Game.start(player)
   end
 end
