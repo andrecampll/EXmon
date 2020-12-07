ca09a8abab75c45810547388a7fc7f3ce7698e61
André <andrevictor50@gmail.com>
2020-12-07 15:12:01 -0300

    correcting game over error

--- lib/exmon.ex
+++ lib/exmon.ex
@@ -18,14 +18,23 @@ defmodule Exmon do
   end
 
   def make_move(move) do
-    move
-    |> Actions.fetch_move()
-    |> do_move()
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
 
     computer_move(Game.info())
   end
 
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
   defp do_move({:ok, move}) do
     case move do
       :move_heal -> Actions.heal()
