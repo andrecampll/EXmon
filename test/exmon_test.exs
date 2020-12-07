7746925f03c49d032e4b4ee9aabeef3716ea9cce
André <andrevictor50@gmail.com>
2020-12-07 16:01:35 -0300

    tests for exmon module

--- test/exmon_test.exs
+++ test/exmon_test.exs
@@ -1,8 +1,37 @@
 defmodule ExmonTest do
   use ExUnit.Case
-  doctest Exmon
 
-  test "greets the world" do
-    assert true == true
+  import ExUnit.CaptureIO
+
+  alias Exmon.Player
+
+  describe "create_player/4" do
+    test "Returns a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{
+          move_avg: :chute,
+          move_heal: :cura,
+          move_rnd: :soco
+        },
+        name: "John Doe"
+      }
+
+      assert Exmon.create_player("John Doe", :soco, :chute, :cura) == expected_response
+    end
+  end
+
+  describe "start_game/1" do
+    test "Returns start game message" do
+      player = Player.build("John Doe", :soco, :chute, :cura)
+
+      messages = capture_io(fn ->
+        assert Exmon.start_game(player) == :ok
+      end)
+
+
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
   end
 end
