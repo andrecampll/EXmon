a1166c7d15120873db9319e4e79b8757941a2ac1
André <andrevictor50@gmail.com>
2020-12-07 10:17:53 -0300

    creating heal actions

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,11 +1,18 @@
 defmodule Exmon.Game.Actions do
   alias Exmon.Game
-  alias Exmon.Game.Actions.Attack
+  alias Exmon.Game.Actions.{Attack, Heal}
 
   def attack(move) do
     case Game.turn() do
       :player -> Attack.attack_opponent(:computer, move)
       :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
+
+  def heal() do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
     end
   end
 
